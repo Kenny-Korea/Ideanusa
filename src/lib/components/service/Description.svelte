ddfe42e7980b76d2789f30988cb9b16f5adbcb50
kenny <kmkim4238@gmail.com>
2024-10-24 01:38:03 +0900

    feat: service 페이지 레이아웃 추가

---
+++ src/lib/components/service/Description.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+</script>
+
+<section>hello</section>
+
+<style lang="scss">
+	section {
+		width: 640px;
+		height: 480px;
+		padding: 1rem;
+		box-sizing: border-box;
+		background-color: yellow;
+	}
+</style>
---
+++ src/routes/service/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import Description from "$lib/components/service/Description.svelte";
+</script>
+
+<section>
+	<img src="/images/service/consulting.jpg" alt="consulting" />
+	<Description />
+	<Description />
+	<img src="/images/service/consulting.jpg" alt="consulting" />
+	<img src="/images/service/consulting.jpg" alt="consulting" />
+	<Description />
+</section>
+
+<style lang="scss">
+	section {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-template-rows: repeat(3, 1fr);
+
+		img {
+			width: 640px;
+			height: 480px;
+			object-fit: cover;
+		}
+	}
+</style>
